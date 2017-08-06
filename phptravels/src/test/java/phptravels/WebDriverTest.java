71fb911800aeafad0a2c6b232af6dcbcc4e962dd
Ulises Martinez <umtz08@gmail.com>
2017-08-05 18:11:25 -0700

    created simple assert test for the site

--- phptravels/src/test/java/phptravels/WebDriverTest.java
+++ phptravels/src/test/java/phptravels/WebDriverTest.java
@@ -1,13 +1,39 @@
 package phptravels;
 
-import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
 
 public class WebDriverTest {
 
+	private WebDriver driver;
+	
+	@BeforeSuite
+	public void initDriver() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\UlisesM\\Downloads\\Drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	
+	@AfterSuite
+	public void quitDriver() {
+		driver.quit();
+	}
+	
+	@BeforeClass
+	public void goToHomePage() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.phptravels.net");
+        Assert.assertEquals("PHPTRAVELS | Travel Technology Partner", driver.getTitle());
+ 
+	}
+	
 	@Test
-	public void checkDummyTest() {
-		System.out.println("Hello world");
-
+	public void login() {
+		
 	}
 
 }
