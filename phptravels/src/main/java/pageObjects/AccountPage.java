c1ec5f4501af5dbf3f8526bd85821e7f4e12e2c2
Ulises Martinez <umtz08@gmail.com>
2017-08-05 19:59:12 -0700

    added login and account page objects

---
+++ phptravels/src/main/java/pageObjects/AccountPage.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccountPage extends PageObject{
+
+	public AccountPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
---
+++ phptravels/src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,42 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends PageObject {
+	
+	@FindBy(name = "username")
+	private WebElement username;
+	
+	@FindBy(name = "password")
+	private WebElement password;
+	
+	@FindBy(xpath = "//button[contains('Login')]")
+	private WebElement submit;
+	
+
+	public LoginPage(WebDriver driver) {	
+		super(driver);
+	}
+	
+	public boolean isInitialized() {
+		return username.isDisplayed();
+	}
+	
+	public void enterUserName(String username) {
+		this.username.clear();
+		this.username.sendKeys(username);
+	}
+	
+	public void enterPassword(String password) {
+		this.password.clear();
+		this.password.sendKeys(password);
+	}
+	
+	public AccountPage submit() {
+		submit.click();
+		return new AccountPage(driver);
+	}
+
+}
