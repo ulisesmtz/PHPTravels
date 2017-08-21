60ced5488561542d556c7ffac6c61e0abb2f7581
Ulises Martinez <umtz08@gmail.com>
2017-08-20 19:25:28 -0700

    added data driven testing for login; ensure failLogin() runs first

--- phptravels/src/test/java/testing/LoginTest.java
+++ phptravels/src/test/java/testing/LoginTest.java
@@ -7,46 +7,48 @@ import pageObjects.AccountPage;
 import pageObjects.LoginPage;
 
 public class LoginTest extends FunctionalTest {
-	
-  /**
- * Logs in with valid credentials and checks that it redirects to the 
- * users account page.
- */
-@Test (priority = 1)
-  public void login() {
-	  driver.get("http://www.phptravels.net/login");
-	  
-	  LoginPage loginPage = new LoginPage(driver);
-	  
-	  loginPage.enterUserName("user@phptravels.com");
-	  loginPage.enterPassword("demouser");
-	  AccountPage accountPage = loginPage.submit();
 
-	  // sleep thread for few seconds so page can redirect
-	  try {
-		Thread.sleep(5000);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
+	/**
+	 * Logs in with valid credentials and checks that it redirects to the users
+	 * account page.
+	 */
+	@Test(dependsOnMethods="failLogin")  // to ensure this method runs second
+	@Parameters({"username", "password"})
+	public void login(String username, String password) {
+		driver.get("http://www.phptravels.net/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.enterUserName(username);
+		loginPage.enterPassword(password);
+		AccountPage accountPage = loginPage.submit();
+
+		// sleep thread for few seconds so page can redirect
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals(accountPage.getTitle(), "My Account");
 	}
-	  Assert.assertEquals(accountPage.getTitle(), "My Account");
-  }
-  
-  /**
- * Attempts to log in with invalid credentials. Checks to make sure error message 
- * is displayed and with correct message.
- */
-@Test (priority = 0)
-  public void failLogin( ) {
-	  driver.get("http://www.phptravels.net/login");
-	  
-	  LoginPage loginPage = new LoginPage(driver);
-	  
-	  loginPage.enterUserName("fake@phptravels.com");
-	  loginPage.enterPassword("fake");
-	  loginPage.submit();
-	  
-	  Assert.assertEquals(loginPage.isErrorVisible(), true);
-	  Assert.assertEquals(loginPage.getErrorMessage(), "Invalid Email or Password");
-  }
-  
+
+	/**
+	 * Attempts to log in with invalid credentials. Checks to make sure error
+	 * message is displayed and with correct message.
+	 */
+	@Test
+	@Parameters({"fakeUsername", "fakePassword"})
+	public void failLogin(String fakeUsername, String fakePassword) {
+		driver.get("http://www.phptravels.net/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.enterUserName(fakeUsername);
+		loginPage.enterPassword(fakePassword);
+		loginPage.submit();
+
+		Assert.assertEquals(loginPage.isErrorVisible(), true);
+		Assert.assertEquals(loginPage.getErrorMessage(), "Invalid Email or Password");
+	}
+
 }
